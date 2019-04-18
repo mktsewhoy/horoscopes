57a2ac24814cd76a9a934d7209241dfd533f430b
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-04-19 10:05:51 +1200

    Refine CSS.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -42,8 +42,10 @@ const App = () => {
               <div class="text"><a href="#">&#9811;</a></div>
             </li>
           </ul>
-        <div class="hole"><h3>sagittarius</h3><h2>&#9808;</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="hole">
+          <h3>sagittarius</h3>
+          <h2>&#9808;</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
   )
