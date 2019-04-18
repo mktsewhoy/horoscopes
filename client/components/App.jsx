e5e1ea2e0591ccf4a18f4ec2ad5b557867535a6f
jorge-vargas-p <jorjato18@gmail.com>
2019-04-19 10:59:46 +1200

    render one horoscope on browser

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import { getHoroscopeInfo } from '../api'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      horoscopes: []
+    }
+
+    this.renderHoroscopeInfo = this.renderHoroscopeInfo.bind(this)
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+    getHoroscopeInfo(this.renderHoroscopeInfo)
+    console.log("Info returned from getHoroscopeInfo: ", getHoroscopeInfo(this.renderHoroscopeInfo)
+    )
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  renderHoroscopeInfo(err, horoscopes) {
+    console.log('rendering horoscope info')
+    this.setState({
+      horoscopes: horoscopes.dailyhoroscope
+    })
+  }
+
+  render() {
+    console.log('render')
+    console.log(this.state.horoscopes)
+    return (
+      <div>
+        <h1>App is rendering!</h1>
+        <p>Horoscopes: {this.state.horoscopes.Aries}</p>
+      </div>
+      // <div>
+      // <h1>Pokemon</h1>
+      // <h2>I have {this.state.pokemon.length} Pokemon!</h2>
+      // {this.state.pokemon.map(p => {
+      //   return (
+      //     <li><a href={p.url}>{p.name}</a></li>
+      //   )
+      // })}
+      // </div>
+    )
+  }
 }
-
 export default App
-
