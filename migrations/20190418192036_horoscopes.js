249b1c4b59c11b0afdc5de07531b22cf40802273
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-04-19 15:23:15 +1200

    changing horoscopes month from integer to string

--- migrations/20190418192036_horoscopes.js
+++ migrations/20190418192036_horoscopes.js
@@ -1,17 +1,14 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('horoscopes',table =>{
-    table.increments('id').primary()
-    table.string('Sun sign')
-    table.string('famous')
-    table.integer('month')
-    table.string('famous Comedy')
-    table.integer('url')
-    
-  })
+  return knex.schema.createTable("horoscopes", table => {
+    table.increments("id").primary();
+    table.string("Sun sign");
+    table.string("famous");
+    table.string("month");
+    table.string("famous Comedy");
+    table.integer("url");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('horoscopes',table)
-  
+  return knex.schema.dropTable("horoscopes", table);
 };
