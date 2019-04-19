05dadd5b9d19d692690e32af823fdd56eb06a78f
runminty <zengzhenhuang@gmail.com>
2019-04-19 14:09:59 +1200

    add ab.js and route folder

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const server = express()
-
+//const horoscopes = require('./routes/horoscopes')
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
