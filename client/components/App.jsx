6b79d48eb4fa63778b40853a379c494aebc8589c
jorge-vargas-p <jorjato18@gmail.com>
2019-04-19 14:10:14 +1200

    add id to horoscope elements

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,71 +14,68 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    console.log('componentWillMount')
     getHoroscopeInfo(this.renderHoroscopeInfo)
     console.log("Info returned from getHoroscopeInfo: ", getHoroscopeInfo(this.renderHoroscopeInfo)
     )
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
   }
 
   renderHoroscopeInfo(err, horoscopes) {
     console.log('rendering horoscope info')
     this.setState({
-      horoscopes: horoscopes.dailyhoroscope
+      horoscopes: horoscopes.dailyhoroscope,
     })
   }
 
   render() {
     console.log('render')
-    console.log(this.state.horoscopes)
     return (
       <div>
         <h1>App is rendering!</h1>
-        <p>Horoscopes: {this.state.horoscopes.Leo}</p>
+        <p>Horoscopes: {this.state.horoscopes.Aries}</p>
         <div>
           <h1>Your Horoscope<br /><br /></h1>
-            <ul class="circle">
-              <li>
-                <div class="text"><a href="#">&#9800;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9801;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9802;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9803;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9804;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9805;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9806;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9807;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9808;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9809;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9810;</a></div>
-              </li>
-              <li>
-                <div class="text"><a href="#">&#9811;</a></div>
-              </li>
-            </ul>
-          <div class="hole">
+          <ul className="circle">
+            <li>
+              <div className="text" id="aries"><a href="#">&#9800;</a></div>
+            </li>
+            <li>
+              <div className="text" id="taurus"><a href="#">&#9801;</a></div>
+            </li>
+            <li>
+              <div className="text" id="gemini"><a href="#">&#9802;</a></div>
+            </li>
+            <li>
+              <div className="text" id="cancer"><a href="#">&#9803;</a></div>
+            </li>
+            <li>
+              <div className="text" id="leo"><a href="#">&#9804;</a></div>
+            </li>
+            <li>
+              <div className="text" id="virgo"><a href="#">&#9805;</a></div>
+            </li>
+            <li>
+              <div className="text" id="libra"><a href="#">&#9806;</a></div>
+            </li>
+            <li>
+              <div className="text" id="scorpio"><a href="#">&#9807;</a></div>
+            </li>
+            <li>
+              <div className="sagittarius"><a href="#">&#9808;</a></div>
+            </li>
+            <li>
+              <div className="text" id="capricorn"><a href="#">&#9809;</a></div>
+            </li>
+            <li>
+              <div className="text" id="aquarius"><a href="#">&#9810;</a></div>
+            </li>
+            <li>
+              <div className="text" id="pisces"><a href="#">&#9811;</a></div>
+            </li>
+          </ul>
+          <div className="hole">
             <h3>sagittarius</h3>
             <h2>&#9808;</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
